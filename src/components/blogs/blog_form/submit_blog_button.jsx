06adac289add7cfab398e524701772792baaabaf
Jay Hwang <jayhwang0121@gmail.com>
2017-07-27 14:51:48 -0700

    remove "publishing..." textfrom submit blog button inactive

--- src/components/blogs/blog_form/submit_blog_button.jsx
+++ src/components/blogs/blog_form/submit_blog_button.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 var Loader = require('react-loaders').Loader;
 
 const SubmitBlogButton = ({ actionType, isActive }) => {
-    return isActive ? (
-        <button id='blog-submit' className='btn skinny'>
-            {actionType} Blog
-        </button>
-    ) : (
-        <button id='blog-submit' className='btn skinny inactive blog-submit-inactive'>
-            Publishing... <Loader type="ball-clip-rotate" active/>
-        </button>
-    )
+  return isActive ? (
+    <button id='blog-submit' className='btn skinny'>
+      {actionType} Blog
+    </button>
+  ) : (
+    <button id='blog-submit' className='btn skinny inactive blog-submit-inactive'>
+      <Loader type="ball-clip-rotate" active/>
+    </button>
+  )
 };
 
 export default SubmitBlogButton;
