8d666652dcc395b10960329b69d884652c0d6f64
Jay Hwang <jayhwang0121@gmail.com>
2017-07-26 20:50:28 -0700

    add headers to webpackdevserver

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -10,6 +10,9 @@ const host = process.env.HOST || '0.0.0.0';
 
 module.exports = function(proxy, allowedHost) {
   return {
+      headers: {
+          'Access-Control-Allow-Origin': '*'
+      },
     // WebpackDevServer 2.4.3 introduced a security fix that prevents remote
     // websites from potentially accessing local content through DNS rebinding:
     // https://github.com/webpack/webpack-dev-server/issues/887
